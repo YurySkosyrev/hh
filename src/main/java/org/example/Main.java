0e4c34bf1dd24773c0431df3da63620e5c720038
YurySkosyrev <skosyrev-87@mail.ru>
2023-08-24 22:39:12 +0300

    another solve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,60 +8,73 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Integer n = scanner.nextInt();
-        Integer m = scanner.nextInt();
-        Integer k = 0;
-        Integer nForRead;
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = 0;
 
-        Integer cnMax = 0;
-        Integer nextValue = 0;
+        short max = 0;
+        short nextValue = 0;
 
-        Boolean check = true;
+        boolean check = true;
 
-        int[] integers = new int[n];
+        int[] cookies = new int[n];
 
-        nForRead = n;
-        while (nForRead > 0) {
-            nextValue = scanner.nextInt();
-            integers[n-nForRead] = nextValue;
-            if (cnMax < nextValue) {
-                cnMax = nextValue;
+        for (int i = 0; i < n; i++) {
+            nextValue = scanner.nextShort();
+            cookies[i] = nextValue;
+            if (max < nextValue) {
+                max = nextValue;
             }
-            nForRead--;
         }
 
-        k = cnMax;
+        k = max;
 
         if (m < n) {
             System.out.println(0);
         } else if (m == n) {
             System.out.println(k);
         } else {
-            while (check) {
-                check = checkList(integers, n, m, k);
-                k -= 1;
-            }
-            System.out.println(k + 2);
+            System.out.println(findMininum(cookies, m, max));
         }
+
     }
 
-    static boolean checkList(int[] array, Integer n, Integer m, Integer k){
+    private static short findMininum(int[] cookies, int m, short max) {
+        short left = 0;
+        short right = max;
+        short mid;
+        short result = 0;
 
-        int node;
-        Integer i = 0;
-
-        while (i < n) {
-            node = array[i];
-            while (node > 0 && m > 0) {
-                m -= 1;
-                node -= k;
+        while (left <= right) {
+            mid = (short)(left + ((right - left) / 2));
+            if (canEatAllCookies(cookies, mid, m)) {
+                result = mid;
+                right = (short)(mid - 1);
+            } else {
+                left  = (short)(mid + 1);
             }
+        }
+        return result;
+    }
 
-            if (node > 0 || m < 0) {
+    private static boolean canEatAllCookies(int[] cookies, short mid, int m) {
+
+        int steps = m;
+
+        if(mid == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < cookies.length; i++) {
+            steps -= cookies[i]/mid;
+            if (cookies[i]%mid > 0) {
+                steps -= 1;
+            }
+            if (steps < 0) {
                 return false;
             }
-            i += 1;
         }
         return true;
     }
+
 }
